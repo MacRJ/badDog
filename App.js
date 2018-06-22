6aeffd1f6f6cb9c1a113d873c576de8330bef2c3
Richard Jones <rmacwj@gmail.com>
2018-06-21 23:30:45 -0700

    added old boilerplatge compoents from privous job

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header.js';
+import AlbumList from './components/oldComponents/AlbumList.js'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header />
+        <AlbumList />
       </View>
     );
   }
@@ -18,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'right',
+    justifyContent: 'center',
   },
-});
+})
